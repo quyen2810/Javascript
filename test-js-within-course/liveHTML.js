8e3eeed96179275a70175857c2bbc9161a3213aa
Quyen Pham <quyenpham021998@gmail.com>
2024-06-21 17:42:20 +0700

    update: init JS file

---
+++ test-js-within-course/liveHTML.js
@@ -0,0 +1,6 @@
+const increaseButtonEl = document.querySelector('.counter__button--increase');
+const counterValueEl = document.querySelector('.counter__value');
+
+increaseButtonEl.addEventListener('click', function(){
+
+});
