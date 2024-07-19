f52879b83b5200c9e8be66eae239ac65189f4734
Quyen Pham <quyenpham021998@gmail.com>
2024-07-19 17:32:13 +0700

    init submit component: get the content from text area

--- feedbackCollecting/script.js
+++ feedbackCollecting/script.js
@@ -1,13 +1,26 @@
-//implement counter component for the input text area
+//global definition
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const maxNumberChars = 150;
+const formEl = document.querySelector('.form');
 
+//implement counter component for the input text area
 const counter = () => {
     const charactersLeft = maxNumberChars - textareaEl.value.length;
     counterEl.textContent = charactersLeft;
 };
-counter ();
+counter();
 textareaEl.addEventListener('input', counter); 
 
+//implement submit component
+const submitHandler = event => {
+    //remove default event from browser
+    event.preventDefault();
+    //console.log(event);
 
+    //get content from text area
+    const text = textareaEl.value;
+    console.log(text);
+};
+
+formEl.addEventListener('submit', submitHandler);
