aab25685f118ca1870b470ee9aeb6cf35745992c
Quyen Pham <quyenpham021998@gmail.com>
2024-07-22 20:45:14 +0700

    validate the input to be with hash and more than 5

--- feedbackCollecting/script.js
+++ feedbackCollecting/script.js
@@ -20,7 +20,25 @@ const submitHandler = event => {
 
     //get content from text area
     const text = textareaEl.value;
-    console.log(text);
+    //console.log(text);
+    //show valid indicator
+    if (text.includes('#') && text.length >= 5) {
+        formEl.classList.add('form--valid');
+        //hide it after 2s
+        const removeValid = () => {
+            formEl.classList.remove('form--valid');
+        };
+        setTimeout(removeValid,2000);
+    } else {
+        //show invalid indicator
+        formEl.classList.add('form--invalid');
+        //hide it after 2s
+        const remove = () => {
+            formEl.classList.remove('form--invalid');
+        };
+        setTimeout(remove,2000);
+    }
+
 };
 
 formEl.addEventListener('submit', submitHandler);
