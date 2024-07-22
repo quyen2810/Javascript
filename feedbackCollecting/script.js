1a31a7f36aa709310eb60b5b040453beb9ea0f69
Quyen Pham <quyenpham021998@gmail.com>
2024-07-22 21:17:23 +0700

    create new HTML item to wrap the extracted info from input text

--- feedbackCollecting/script.js
+++ feedbackCollecting/script.js
@@ -10,7 +10,7 @@ const counter = () => {
     counterEl.textContent = charactersLeft;
 };
 counter();
-textareaEl.addEventListener('input', counter); 
+textareaEl.addEventListener('input', counter);
 
 //implement submit component
 const submitHandler = event => {
@@ -28,7 +28,7 @@ const submitHandler = event => {
         const removeValid = () => {
             formEl.classList.remove('form--valid');
         };
-        setTimeout(removeValid,2000);
+        setTimeout(removeValid, 2000);
     } else {
         //show invalid indicator
         formEl.classList.add('form--invalid');
@@ -36,7 +36,7 @@ const submitHandler = event => {
         const remove = () => {
             formEl.classList.remove('form--invalid');
         };
-        setTimeout(remove,2000);
+        setTimeout(remove, 2000);
         //focus on text area again to fix the invalid
         textareaEl.focus();
         //stop executing when it's invalid, return nothing = stop
@@ -48,10 +48,28 @@ const submitHandler = event => {
     // remove the has from hashtag
     const company = hashtag.substring(1);
     // take the 1st character
-    const badgeLetter = company.substring(0,1).touppercase();
+    const badgeLetter = company.substring(0, 1).touppercase();
     const upvoteCount = 0;
     const daysAgo = 0;
+
+    //create new html for new feedback
+    const feedbackItemHTML = `
+        <li class="feedback">
+         <button class="upvote">
+             <i class="fa-solid fa-caret-up upvote__icon"></i>
+            <span class="upvote__count">${upvoteCount}</span>
+        </button>
+        <section class="feedback__badge">
+             <p class="feedback__letter">${badgeLetter}</p>
+         </section>
+         <div class="feedback__content">
+            <p class="feedback__company">${company}</p>
+             <p class="feedback__text">${text}</p>
+        </div>
+        <p class="feedback__date">${daysAgo}</p>
+        </li>
     `;
+    
 };
 
 formEl.addEventListener('submit', submitHandler);
