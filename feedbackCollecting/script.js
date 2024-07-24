2cc73818ad287729c74a5273fa4be0a3a3a0a712
Quyen Pham <quyenpham021998@gmail.com>
2024-07-24 11:32:13 +0700

    clear text, lose focus = blur on submit button, let the counter back to original

--- feedbackCollecting/script.js
+++ feedbackCollecting/script.js
@@ -4,6 +4,7 @@ const counterEl = document.querySelector('.counter');
 const maxNumberChars = 150;
 const formEl = document.querySelector('.form');
 const feedbackListEl = document.querySelector('.feedbacks');
+const submitBtnEl = document.querySelector('.submit-btn');
 
 //implement counter component for the input text area
 const counter = () => {
@@ -25,6 +26,7 @@ const submitHandler = event => {
     //show valid indicator
     if (text.includes('#') && text.length >= 5) {
         formEl.classList.add('form--valid');
+
         //hide it after 2s
         const removeValid = () => {
             formEl.classList.remove('form--valid');
@@ -33,21 +35,26 @@ const submitHandler = event => {
     } else {
         //show invalid indicator
         formEl.classList.add('form--invalid');
+
         //hide it after 2s
         const remove = () => {
             formEl.classList.remove('form--invalid');
         };
         setTimeout(remove, 2000);
+
         //focus on text area again to fix the invalid
         textareaEl.focus();
+
         //stop executing when it's invalid, return nothing = stop
         return;
     }
     //add the input valid text to the list, extract info from submit text
     const hashtag = text.split(' ').find(word => word.includes('#'));
     //console.log(text.split(' ').find(word => word.includes('#')));
+
     // remove the has from hashtag
     const company = hashtag.substring(1);
+
     // take the 1st character
     const badgeLetter = company.substring(0, 1).toUpperCase();
     const upvoteCount = 0;
@@ -67,11 +74,16 @@ const submitHandler = event => {
             <p class="feedback__company">${company}</p>
              <p class="feedback__text">${text}</p>
         </div>
-        <p class="feedback__date">${daysAgo}</p>
+        <p class="feedback__date">${daysAgo === 0 ? 'NEW' : `${daysAgo}d`}</p>
         </li>
     `;
     //show new feedback item in the list
     feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+
+    //clear text, lose focus = blur on submit button, let the counter back to 150
+    textareaEl.value = '';
+    submitBtnEl.blur();
+    counterEl.textContent = 150;
 };
 
 formEl.addEventListener('submit', submitHandler);
