982622c42fd0bb1217ec7e22464bf516f9ef3ce8
Quyen Pham <quyenpham021998@gmail.com>
2024-08-08 17:13:28 +0700

    refactor these scripts

--- feedbackCollecting/script.js
+++ feedbackCollecting/script.js
@@ -1,142 +1,130 @@
-//global definition
+// -- GLOBAL --
 const MAX_CHARS = 150;
-const BASE_API_URL = 'https://bytegrad.com/course-assets/js/1/api/feedbacks';
 
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
-const maxNumberChars = MAX_CHARS;
 const formEl = document.querySelector('.form');
 const feedbackListEl = document.querySelector('.feedbacks');
 const submitBtnEl = document.querySelector('.submit-btn');
+const spinnerEl = document.querySelector('.spinner');
 
-//implement counter component for the input text area
-const counter = () => {
-    const charactersLeft = maxNumberChars - textareaEl.value.length;
-    counterEl.textContent = charactersLeft;
+const renderFeedbackItem = feedbackItem => {
+    // new feedback item HTML
+    const feedbackItemHTML = `
+        <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${feedbackItem.company}</p>
+                <p class="feedback__text">${feedbackItem.text}</p>
+            </div>
+            <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
+        </li>
+    `;
+
+    // insert new feedback item in list
+    feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
 };
-counter();
-textareaEl.addEventListener('input', counter);
 
-//implement submit component
-const showValidIndicator = (textCheck) => {
+
+// -- COUNTER COMPONENT --
+const inputHandler = () => {
+    // determine maximum number of characters
+    const maxNrChars = MAX_CHARS;
+
+    // determine number of characters currently typed
+    const nrCharsTyped = textareaEl.value.length;
+
+    // calculate number of characters left (maximum minus currently typed)
+    const charsLeft = maxNrChars - nrCharsTyped;
+
+    // show number of characters left
+    counterEl.textContent = charsLeft;
+};
+
+textareaEl.addEventListener('input', inputHandler);
+
+
+// -- FORM COMPONENT --
+const showVisualIndicator = textCheck => {
     const className = textCheck === 'valid' ? 'form--valid' : 'form--invalid';
+
+    // show valid indicator
     formEl.classList.add(className);
-    //hide it after 2s
-    const removeValid = () => {
+
+    // remove visual indicator
+    setTimeout(() => {
         formEl.classList.remove(className);
-    };
-    setTimeout(removeValid, 2000);
-}
+    }, 2000);
+};
+
 const submitHandler = event => {
-    //remove default event from browser
+    // prevent default browser action (submitting form data to 'action'-address and refreshing page)
     event.preventDefault();
 
-    //get content from text area
+    // get text from textarea
     const text = textareaEl.value;
-
-    //show valid indicator
+    
+    // validate text (e.g. check if #hashtag is present and text is long enough)
     if (text.includes('#') && text.length >= 5) {
-        showValidIndicator('valid');
+        showVisualIndicator('valid');
     } else {
-        showValidIndicator('invalid');
+        showVisualIndicator('invalid');
 
-        //focus on text area again to fix the invalid
+        // focus textarea
         textareaEl.focus();
 
-        //stop executing when it's invalid, return nothing = stop
+        // stop this function execution
         return;
     }
-    //add the input valid text to the list, extract info from submit text
+
+    // we have text, now extract other info from text
     const hashtag = text.split(' ').find(word => word.includes('#'));
-
-    // remove the has from hashtag
     const company = hashtag.substring(1);
-
-    // take the 1st character
     const badgeLetter = company.substring(0, 1).toUpperCase();
     const upvoteCount = 0;
     const daysAgo = 0;
 
-    //create new html for new feedback
-    const feedbackItemHTML = `
-        <li class="feedback">
-         <button class="upvote">
-             <i class="fa-solid fa-caret-up upvote__icon"></i>
-            <span class="upvote__count">${upvoteCount}</span>
-        </button>
-        <section class="feedback__badge">
-             <p class="feedback__letter">${badgeLetter}</p>
-         </section>
-         <div class="feedback__content">
-            <p class="feedback__company">${company}</p>
-             <p class="feedback__text">${text}</p>
-        </div>
-        <p class="feedback__date">${daysAgo === 0 ? 'NEW' : `${daysAgo}d`}</p>
-        </li>
-    `;
-    //show new feedback item in the list
-    feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+    // create feedback item object
+    const feedbackItem = {
+        upvoteCount: upvoteCount,
+        company: company,
+        badgeLetter: badgeLetter,
+        daysAgo: daysAgo,
+        text: text
+    };
 
-    //send feedback item to server
-    fetch(`${BASE_API_URL}`, {
-        method: 'POST',
-        body: JSON.stringify(feedbackItemHTML),
-        headers: {
-            'Content-Type': 'application/json',
-            accept: 'application/json'
-        }
-    }).then(response => {
-        if (!response.ok) {
-            console.log('Failed to send feedback');
-            return;
-        } else {
-            console.log('Feedback sent');
-        }
-        return response.json();
-    }).catch(error => {
-        console.log(error);
-    });
+    // render feedback item
+    renderFeedbackItem(feedbackItem);
 
-    //clear text, lose focus = blur on submit button, let the counter back to 150
+    // clear textarea
     textareaEl.value = '';
+
+    // blur submit button
     submitBtnEl.blur();
+
+    // reset counter
     counterEl.textContent = MAX_CHARS;
 };
 
 formEl.addEventListener('submit', submitHandler);
 
-//add the feedback list under the submission form
-//convert the data from JSON to Javascript
-fetch(`${BASE_API_URL}`).then(response => {
-    return response.json();
-}).then(data => {
-    //looping over the array 
-    data.feedbacks.forEach(feedbackItem => {
-        //create new html for new feedback
-        const daysAgo = 0; //TODO: investigate why I have to define this only variable again
-        const feedbackItemHTML = `
-        <li class="feedback">
-        <button class="upvote">
-        <i class="fa-solid fa-caret-up upvote__icon"></i>
-        <span class="upvote__count">${feedbackItem.upvoteCount}</span>
-        </button>
-        <section class="feedback__badge">
-        <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
-        </section>
-        <div class="feedback__content">
-        <p class="feedback__company">${feedbackItem.company}</p>
-        <p class="feedback__text">${feedbackItem.text}</p>
-        </div>
-        <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${daysAgo}d`}</p>
-        </li>
-        `;
-        //show new feedback item in the list
-        feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+
+// -- FEEDBACK LIST COMPONENT --
+fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
+    .then(response => response.json())
+    .then(data => {
+       // iterate over each element in feedbacks array and render it in list
+        data.feedbacks.forEach(feedbackItem => renderFeedbackItem(feedbackItem));
+    })
+    .catch(error => {
+        feedbackListEl.textContent = `Failed to fetch feedback items. Error message: ${error.message}`;
     });
-})
-.catch(error => {
-    feedbackListEl.textContent = `Failed to fetch feedbacks. Error message: ${error.message}`;
-});
 
 //add click event to the feedback item
 feedbackListEl.addEventListener('click', clickHandler = event => {
